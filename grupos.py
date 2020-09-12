95ad2f65fde99cd28c96623083937a7e5a13076d
Luis Mesajil <lhmesajil@gmail.com>
2020-09-12 15:11:00 -0500

    A grupos.py
    Algoritmo funcional de cálculo de subgrupos

---
+++ grupos.py
@@ -0,0 +1,24 @@
+
+def calSubGrupos (lista = [], nSubGrupos = 0):
+    if (nSubGrupos <= 0): return []
+    if (nSubGrupos == 1): return [[e] for e in lista]
+    subGruposSubLista = calSubGrupos (lista = lista[1:], nSubGrupos= nSubGrupos - 1)
+    # print ("nSubGrupos: ", nSubGrupos, "subGruposSubLista = ", subGruposSubLista)
+    subGrupos = []
+    for i in range(len(subGruposSubLista)):
+        # print (nSubGrupos, ": ", [subGrupo for subGrupo in subGruposSubLista[i:]])
+        subGrupos.append ([[lista[i]] + subGrupo for subGrupo in subGruposSubLista[i:]])
+    return subGrupos
+
+
+lista = ["React", "Ionic", "Xamarin", "Java"]
+lista = ['a','b','c','d','e']
+nSubGrupos = int (input ("Ingrese numero de sub-grupos: "))
+
+
+subGrupos = calSubGrupos (lista = lista, nSubGrupos = nSubGrupos)
+
+for subGrupo in subGrupos:
+    print (subGrupo)
+# print (subGrupos)
+
