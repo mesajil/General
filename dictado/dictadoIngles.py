3d2c6ba91dc0e4e13a1b3b8603884e563e0d5d5f
Luis Mesajil <lhmesajil@gmail.com>
2020-10-05 16:13:43 -0500

    M dictado/dictadoIngles +Menu de dictado

--- dictado/dictadoIngles.py
+++ dictado/dictadoIngles.py
@@ -7,4 +7,17 @@ def returnRandomLetters (n):
         str += random.choice(string.ascii_uppercase)
     return str
 
+def desplegarMenu ():
+    print ("Presione enter para siguiente dictado, n para salir")
 
+
+op = ""
+print ("Ingrese numero de letras a generar: ")
+n = int (input())
+
+while (op != "n"):
+    desplegarMenu ()
+    op = input()
+    if (op != ""):
+        continue
+    print (returnRandomLetters(n), "\n\n")
