734c078d7eb59b363418600cf63524aa280adfde
Luis Mesajil <lhmesajil@gmail.com>
2020-10-18 22:24:31 -0500

    M Math/Ecuacion2
    Definir f como la función a utilizar

--- math/Ecuacion2.py
+++ math/Ecuacion2.py
@@ -1,12 +1,9 @@
 from math import sqrt
-def ecuacion (x):
-    return pow(x,5) + pow(x,4) + 1
+def f (x):
+    return (pow(x,2) - 3) / (3 - x)
 
-def xGrado3 (p,q):
-    a = -q/2
-    b = sqrt(pow(q,2)/4 + pow(p,3)/27)
-    return ((a + b) ** 1/3) + ((a - b) ** 1/3)
-g = xGrado3(-1,1)
-print (g)
-print (ecuacion(g))
+
+if __name__ == "__main__":
+    x = 3.333
+    print (f(x))
 
