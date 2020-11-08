502077063f7ab6d549dfa885b83fac7e8b5653f8
Luis Mesajil <lhmesajil@gmail.com>
2020-11-08 18:59:40 -0500

    M Curso/Ventas/Main +Read +Update client(s)

--- curso/Ventas/Main.py
+++ curso/Ventas/Main.py
@@ -1,20 +1,22 @@
 
 
 
-clients = "Pablo,Ricardo,"
+clients = "Luis,Cristian,Angel,Betsabe,Piero,"
 
-def __add_coma ():
+def _add_coma ():
     global clients
     clients += ","
 
-def __print_welcome ():
+def _print_welcome ():
     print ("WELCOME TO LM VENTAS")
     print ("*" * 20)
 
 
-def __print_menu ():
+def _print_menu ():
     print ("What would you like to do today ?")
     print ("[C]reate client")
+    print ("[R]ead client's list")
+    print ("[U]pdate client")
     print ("[D]elete client")
 
 def list_clients ():
@@ -23,25 +25,52 @@ def list_clients ():
         print(c)
 
 
+def update_client (client_name, updated_client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name, updated_client_name)
+    else:
+        print ("'{}' is not in the client's list".format(client_name))
+
 def create_client (client_name):
     global clients
     if client_name not in clients:
         clients += client_name
-        __add_coma()
+        _add_coma()
     else:
-        print ("Client already is in the Client's list")
+        print ("'{}' already is in the Client's list".format(client_name))
+
+def delete_client (client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ",", "")
+    else:
+        print ("'{}' is not in the client's list".format(client_name))
+    
+    
+def _get_client_name ():
+    return input ("What is the client name?: ")
+
+def _get_updated_client_name ():
+    return input ("What is the updated client name?: ")
 
 if __name__ == "__main__":
-    __print_welcome()
+    _print_welcome()
 
     while (1):
-        __print_menu()
-        cmd = input ("")
+        _print_menu()
+        cmd = input ("").upper()
         if (cmd == "C"):
-            client_name = input ("What is the client name?: ")
+            client_name = _get_client_name()
             create_client (client_name)
+        elif cmd == "R":
             list_clients()
+        elif (cmd == "U"):
+            client_name = _get_client_name()
+            updated_client_name = _get_updated_client_name()
+            update_client (client_name, updated_client_name)
         elif (cmd == "D"):
-            pass
+            client_name = _get_client_name()
+            delete_client (client_name)
         else:
             print ("Invalid command")
