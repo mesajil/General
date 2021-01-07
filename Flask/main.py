c5a1bb18ac040956457d46414ad9dcaedbc9ed13
Luis Mesajil <lhmesajil@gmail.com>
2021-01-06 22:39:25 -0500

    Templates con flask

--- Flask/main.py
+++ Flask/main.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 app = Flask(__name__)
 
 
 @app.route('/') # RUTA RAIZ
 def index ():
-    reponse = make_response (redirect("/hello"))
-    reponse.set_cookie ("user_ip", request.remote_addr) # CREANDO NUEVA COOKIE
-    return reponse # RETORNO DE RESPUESTA DE FLASK
+    response = make_response (redirect("/hello"))
+    # Creando cookie con nombre user_ip y se almacena la dirección ip del cliente (remote_addr)
+    response.set_cookie ("user_ip", request.remote_addr)
+    return response # RETORNO DE RESPUESTA DE FLASK
 
 @app.route('/hello') # RUTA HELLO
 def hello():
-    return "IP = {}".format(request.cookies.get("user_ip"))
+    text = "IP = {}, name = {}".format(request.cookies.get("user_ip"), __name__)
+    return render_template ("hello.html", text = text)
